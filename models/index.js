3ec1f8921ad4a021d8b575c9c2de873bd831d23b
Anna <annamp727@gmail.com>
2021-10-28 21:31:18 -0500

    Completed helpfulness, reviews, and report routes

--- models/index.js
+++ models/index.js
@@ -3,20 +3,42 @@ const db = require('../db/index.js');
 module.exports = {
   reviews: {
     getReviews: function (page, count, sort, product_id) {
-      console.log('the request coming into the model: ', typeof product_id)
-      return new Promise((resolve, reject) => {
+      //console.log('the request coming into the model: ', typeof product_id)
+      return new Promise(async (resolve, reject) => {
         let text = 'SELECT * FROM getTheReviews($1, $2, $3)'
         let values = [page, count, product_id]
-        db
-          .query(text, values)
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((error) => {
-            reject(error);
-          })
+        // db
+        //   .query(text, values)
+        //   .then((res) => {
+        //     resolve(res);
+        //   })
+        //   .catch((error) => {
+        //     reject(error);
+        //   })
+        try {
+          const reviewsResponse = await db.query(text, values)
+          let photoPromises = [];
+          //console.log('is this going to print or nah --------------->', reviewsResponse.rows)
+          let reviews = reviewsResponse.rows;
+          for (var i = 0; i < reviews.length; i++) {
+            let reviewId = reviews[i].id;
+            //console.log('this is the review id -------------->:', reviewId)
+            let photosText = 'SELECT id, url FROM photos WHERE reviews_id = $1'
+            let photosValues = [reviewId];
+            photoPromises.push(db.query(photosText, photosValues))
+          }
+          let photos = await Promise.all(photoPromises);
+          for (var j = 0; j < photos.length; j++) {
+            //console.log('did this run ------------------------------->')
+            reviews[j].photos = photos[j].rows;
+          }
+          //console.log('this is the final review obj: ', reviews)
+          resolve(reviews)
+        } catch (err) {
+          reject(err);
+        }
       })
-    }
+    },
     // getMetaData: function () {
     //   const promise = new Promise((resolve, reject) => {
     //     //do stuff
@@ -24,21 +46,61 @@ module.exports = {
     //     //query
     //   })
     // },
-    // postReview: function () {
-    //   const promise = new Promise((resolve, reject) => {
+    postReview: function () {
+      // const promise = new Promise((resolve, reject) => {
+      //   //product_id	integer	Required ID of the product to post the review for
+      //   // rating	int	Integer (1-5) indicating the review rating
+      //   // summary	text	Summary text of the review
+      //   // body	text	Continued or full text of the review
+      //   // recommend	bool	Value indicating if the reviewer recommends the product
+      //   // name	text	Username for question asker
+      //   // email	text	Email address for question asker
+      //   // photos	[text]	Array of text urls that link to images to be shown
+      //   // characteristics	object	Object of keys representing characteristic_id and values representing the review value for that characteristic. { "14": 5, "15": 5 //...}
+      //   let reviewsText = 'INSERT INTO reviews()';
+      //   let reviewsValues = '';
 
-    //   })
-    // },
-    // updateHelpful: function () {
-    //   const promise = new Promise((resolve, reject) => {
+      //   let photosText = '';
+      //   let photosValues = '';
 
-    //   })
-    // },
-    // updateReport: function () {
-    //   const promise = new Promise((resolve, reject) => {
+      //   let characteristicsText = '';
+      //   let characteristicsValues = '';
+      //   db
+      //     .query(text, values)
+      //     .then(res => {
+      //       console.log(res.rows[0])
+      //       // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+      //     })
+      //     .catch(e => console.error(e.stack))
+      // })
+    },
+    updateHelpful: function (id) {
+      return new Promise(async (resolve, reject) => {
+        let helpfulText = "UPDATE reviews SET helpfulness = helpfulness + 1 WHERE id = $1";
+        let helpfulValue = [id];
 
-    //   })
-    // }
-  }
+        try {
+          const updateHelpfulnessResponse = await db.query(helpfulText, helpfulValue);
+          resolve();
+        } catch (err) {
+          reject(err)
+        }
+      })
+    },
+    updateReport: function (id) {
+      return new Promise(async (resolve, reject) => {
+        let reportText = "UPDATE reviews SET reported = 'True' WHERE id = $1";
+        let reportValue = [id];
+
+        try {
+          const updateResponse = await db.query(reportText, reportValue);
+          resolve()
+        } catch (err) {
+          reject(err)
+        }
+      })
+    }
+  },
+  db
 }
 
--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-const models = require('../models/index.js');
+const { reviews, db } = require('../models/index.js');
 
 app.get('/reviews', (req, res) => {
   let page = 1;
   let count = 5;
   let sort = 'newest';
-  let product_id = '5';
-  models.reviews.getReviews(page, count, sort, product_id)
+  let product_id = req.query.product_id;
+  reviews.getReviews(page, count, sort, product_id)
     .then((response) => {
-      console.log('success getting reviews: ', response.rows);
-      res.send(response.rows);
+      console.log('success getting reviews: ', response);
+      res.send(response);
     })
     .catch((error) => {
       console.log('error getting reviews: ', error);
@@ -27,13 +27,31 @@ app.get('/reviews', (req, res) => {
 
 // });
 
-// app.put('/reviews/:review_id/helpful', (req, res) => {
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  let id = req.params.review_id;
+  reviews.updateHelpful(id)
+    .then((response) => {
+      console.log('successfully marked as helpful');
+      res.sendStatus(204);
+    })
+    .catch((error) => {
+      console.log('error updating helpfulness: ', error);
+      res.sendStatus(500);
+    })
+});
 
-// });
-
-// app.put('/reviews/:review_id/report', (req, res) => {
-
-// });
+app.put('/reviews/:review_id/report', (req, res) => {
+  let id = req.params.review_id;
+  reviews.updateReport(id)
+    .then((response) => {
+      console.log('successfully reported');
+      res.sendStatus(204);
+    })
+    .catch((error) => {
+      console.log('report not processed: ', error);
+      res.sendStatus(500);
+    })
+});
 
 
 
@@ -42,5 +60,10 @@ app.get('/reviews', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 })
+// let close = () => {
+//   server.close(() => {
+//     db.close();
+//   });
+// }
 
 module.exports = app;
